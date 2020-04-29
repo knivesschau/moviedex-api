7b1aba3fa33a202e95132ab9e274fccf4c3af1f3
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-04-28 17:13:50 -0700

    set up server and tested basic startup

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('hello!')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIEDATA = require('./movie-data.json');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send("This is a server test!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started listening at http://localhost:${PORT}!`);
+})
